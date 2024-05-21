0f576faee390473cf87863c1f16b1e7ad18747e9
Fleurdelys2 <98351679+Fleurdelys2@users.noreply.github.com>
2024-05-21 15:15:58 -0400

    cadenas

--- Fragments of the past/Assets/Scripts/Cadenas.cs
+++ Fragments of the past/Assets/Scripts/Cadenas.cs
@@ -8,10 +8,12 @@ public class Cadenas : MonoBehaviour
     [SerializeField] private GameObject cadenaGris;
     [SerializeField] private GameObject cadenaOr;
     [SerializeField] private Animator joueur;
+    [SerializeField] private ParticleSystem explosion;
 
     private bool isCle1 = false;
     private bool isCle2 = false;
     private bool isCle3 = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,18 +32,27 @@ public class Cadenas : MonoBehaviour
         {
             if (collision.gameObject.GetComponent<ControleJoueur>().getType().Contains(CollectionnableEnum.Cle_argent))
             {
+                if (!isCle1)
+                    Instantiate(explosion, cadenaBronze.transform.position, Quaternion.identity);
                 isCle1 = true;
                 cadenaBronze.GetComponent<Animator>().SetBool("Unlocked", true);
+                
             }
             if (collision.gameObject.GetComponent<ControleJoueur>().getType().Contains(CollectionnableEnum.Cle_bronze))
             {
+                if(!isCle2)
+                    Instantiate(explosion, cadenaGris.transform.position, Quaternion.identity);
                 isCle2 = true;
                 cadenaGris.GetComponent<Animator>().SetBool("Unlocked", true);
+                
             }
             if (collision.gameObject.GetComponent<ControleJoueur>().getType().Contains(CollectionnableEnum.Cle_or))
             {
+                if (!isCle3)
+                    Instantiate(explosion, cadenaOr.transform.position, Quaternion.identity);
                 isCle3 = true;
                 cadenaOr.GetComponent<Animator>().SetBool("Unlocked", true);
+                
             }
 
             if(isCle1 && isCle2 && isCle3)
